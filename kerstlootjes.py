ef2c84b0b43fe4b23b428df46b277f98edc4196f
AmarNagim <89778585+AmarNagim@users.noreply.github.com>
2022-01-21 10:00:52 +0100

    kerstlootjes finished

--- kerstlootjes.py
+++ kerstlootjes.py
@@ -2,37 +2,49 @@ import random
 
 print('Welkom bij kerstlootjes.\n')
 
-
 def nameLoop():
     count = 1
     usersValidation = True
     userList = []
     while usersValidation:
         users = input(f'Vul hier de naam in van deelnemer {count}: ')
+        while users in userList:
+            users = input(f'Er bestaat al een lootje met deze naam, vul alstublieft een andere naam in voor deelnemer {count}: ')
+            
         userList.append(users)
         count+=1
-        yesNo = input('Wil je nog een naam toevoegen? Y/N: ').lower()
-        if yesNo == 'n':
-            usersValidation = False
-        elif yesNo == 'y':
-            pass    
-        else: 
-            print('Ik heb u Helaas niet begrepen')
-    return count, usersValidation, userList, yesNo, users
+        if (count % 2):
+            yesNo = input('Wil je nog een naam toevoegen? Y/N: ').lower()
+            if yesNo == 'n':
+                usersValidation = False
+            elif yesNo == 'y':
+                pass    
+            while yesNo != 'y' and yesNo != 'n':
+                        print('Ik heb u Helaas niet begrepen')
+                        yesNo = input('Wil je nog een naam toevoegen? Y/N: ').lower()
+                        if yesNo == 'n':
+                            usersValidation = False
+                        elif yesNo == 'y':
+                            pass    
+    return count, usersValidation, userList, users
 
-count, userValidation, userList, yesNo, users = nameLoop()
-print(userList)
-count2 = 1
-listPosition = 0
-amountUsers = count-2
-print(amountUsers)
-while len(userList) > 0:
-    randomPick = random.randint(count2, amountUsers)
-    print(randomPick)
-    pull = print(f'\n{userList[count2]} trekt een lootje..\n')
-    count2+=1
-    listPosition+=1
-    print(f'Op het lootje staat.. {userList[randomPick]}')
-    userList.pop(randomPick-1)
-    continueOrNot = input('Druk ENTER om door te gaan naar de volgende deelnemer..')
-        
+count, userValidation, userList, users = nameLoop()
+random.shuffle(userList)
+
+def randInt():
+    amountNames = len(userList)-1
+    randomInt = random.randint(0, amountNames)
+    return randomInt
+
+while len(userList) >= 2:
+    # print(userList)
+    number = randInt()
+    numberName = userList[number]
+    userList.pop(number)
+    number2 = randInt()
+    number2Name = userList[number2]
+    userList.pop(number2)
+    print(f'\n{numberName} heeft {number2Name} getrokken\n')
+    input('Druk ENTER om door te gaan\n')
+    
+    
